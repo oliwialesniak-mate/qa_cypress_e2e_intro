59d79138fe10f239773c71dd4651e67d16b6fafc
Oliwia Lesniak <oliwialesniak666@gmail.com>
2025-09-30 12:30:58 +0200

    Secure Cypress Sign In test using env variables

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,34 +1,30 @@
 /// <reference types="cypress" />
 
-// eslint-disable-next-line n/handle-callback-err
-Cypress.on('uncaught:exception', (err, runnable) => {
-  return false;
-});
-
 describe('Sign In page', () => {
   it('should log in and display username in the header', () => {
-    // Intercept the login request
+    // Intercept the login API
     cy.intercept('POST', '**/users/login').as('loginRequest');
 
     // Visit homepage
     cy.visit('https://react-redux.realworld.io');
 
-    // Click "Sign in"
-    cy.contains('Sign in').click();
+    // Click "Sign in" link specifically
+    cy.contains('a', 'Sign in').click();
 
-    // Fill in email and password
-    cy.get('input[type="email"]').type('qa.tester@mate.academy');
-    cy.get('input[type="password"]').type('Qwerty123!');
+    // Fill credentials from Cypress.env
+    cy.get('input[type="email"]').type(Cypress.env('USER_EMAIL'));
+    cy.get('input[type="password"]').type(Cypress.env('USER_PASSWORD'));
 
     // Submit login
     cy.get('button[type="submit"]').contains('Sign in').click();
 
-    // Wait for login request to finish
-    cy.wait('@loginRequest');
+    // Assert login request response
+    cy.wait('@loginRequest')
+      .its('response.statusCode')
+      .should('eq', 200);
 
-    // Assert that username appears in the header (navbar)
-    cy.get('nav')
-      .contains('QaTester')
-      .should('be.visible');
+    // Assert username in navbar
+    const expectedName = Cypress.env('USER_NAME');
+    cy.get('nav').contains(expectedName).should('be.visible');
   });
 });
